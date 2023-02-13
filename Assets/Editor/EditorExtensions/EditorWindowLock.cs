07e92c5c06ce7bee49f204f4761f18ba171b8d1c
Roman Molotov <r.molotov@yahoo.com>
2023-02-13 17:59:52 +0400

    editor extension: lock inspector / project panel

---
+++ Assets/Editor/EditorExtensions/EditorWindowLock.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor.EditorExtensions
+{
+    public static class EditorWindowLock
+    {
+        private static EditorWindow _activeProject;
+ 
+        [MenuItem("Edit/Custom shortcuts/Toggle Lock %SPACE")]
+        private static void ToggleInspectorLock()
+        {
+            ActiveEditorTracker.sharedTracker.isLocked = !ActiveEditorTracker.sharedTracker.isLocked;
+            ActiveEditorTracker.sharedTracker.ForceRebuild();
+        }
+    
+        [MenuItem("Edit/Custom shortcuts/Toggle Project Lock %#SPACE")]
+        private static void ToggleProjectLock()
+        {
+            if (_activeProject == null)
+            {
+                var type = Assembly.GetAssembly(typeof(UnityEditor.Editor)).GetType("UnityEditor.ProjectBrowser");
+                var findObjectsOfTypeAll = Resources.FindObjectsOfTypeAll(type);
+                _activeProject = (EditorWindow)findObjectsOfTypeAll[0];
+            }
+
+            if (_activeProject == null || _activeProject.GetType().Name != "ProjectBrowser") return;
+            {
+                var type = Assembly.GetAssembly(typeof(UnityEditor.Editor)).GetType("UnityEditor.ProjectBrowser");
+                var propertyInfo = type.GetProperty("isLocked", BindingFlags.Instance | 
+                                                                BindingFlags.NonPublic |
+                                                                BindingFlags.Public);
+        
+                var value = (bool)propertyInfo.GetValue(_activeProject, null);
+      
+                propertyInfo.SetValue(_activeProject, !value, null);
+      
+                _activeProject.Repaint();
+            }
+        }
+    }
+}
