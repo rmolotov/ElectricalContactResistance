582fe56cd4001fac83ce689a6b007b08a33abc8e
Roman Molotov <r.molotov@yahoo.com>
2023-03-13 13:34:23 +0400

    trigger observer component

---
+++ Assets/ECR/Gameplay/Logic/TriggerObserver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace ECR.Gameplay.Logic
+{
+    [RequireComponent(typeof(Collider))]
+    public class TriggerObserver : MonoBehaviour
+    {
+        public event Action<Collider> TriggerEnter;
+        public event Action<Collider> TriggerExit;
+
+        private void OnTriggerEnter(Collider other) =>
+            TriggerEnter?.Invoke(other);
+
+        private void OnTriggerExit(Collider other) =>
+            TriggerExit?.Invoke(other);
+    }
+}
